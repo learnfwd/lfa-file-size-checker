ea8a368f9375e9e38e190ea025ef97f07b298554
Marius Petcu <dapetcu21@gmail.com>
2015-07-08 14:13:05 +0300

    Added fix suggestion

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ var tap = require('gulp-tap');
 var _ = require('lodash');
 var chalk = require('chalk');
 var gm = require('gm');
+var when = require('when');
+var nodefn = require('when/node');
+var path = require('path');
+var fs = require('fs');
 
 var fileActions = {
   '\\.(jpe?g|png)$': { 
     maxSize: 1000000,
     image: true,
-    minWidth: 640,
-    minHeight: 480,
     maxWidth: 2048,
     maxHeight: 1536,
   },
@@ -17,13 +19,57 @@ var fileActions = {
   '\\.(mp3|ogg|aac|m4a)': { maxSize: 2000000},
 };
 
+function parseNumber(str) {
+  try {
+    return parseFloat(str.match(/\((.*)\)/)[1]);
+  } catch (ex) {
+    return NaN;
+  }
+}
+
 
 module.exports = function fileSizeChecker(lfa) {
   function warn(file, msg) {
     var err = new Error(msg);
-    err.fileName = file.path;
+    err.fileName = file.history[0];
     err.nameLess = true;
     lfa.logWarning(err);
+  }
+
+  var fixes = [];
+
+  function applyFix(file, fix) {
+    if (!fix) { return; }
+
+    var fileName = file.history[0];
+    var assetsPath = path.join(lfa.config.projectPath, 'assets');
+    if (fileName.indexOf(assetsPath) !== 0) { return; }
+
+    if (fix === 'pngcrush') {
+      fixes.push('pngcrush -rem gAMA -rem cHRM -rem iCCP -rem sRGB "' + fileName + '" "' + fileName + '.crushed.png"');
+      fixes.push('mv "' + fileName + '.crushed.png" "' + fileName + '"');
+      return;
+    }
+
+    if (fix === 'tojpg') {
+      var jpegPath = fileName.replace(/\.png$/, '.jpg');
+      var relativePath = path.relative(assetsPath, fileName);
+      var relativeJpegPath= relativePath.replace(/\.png$/, '.jpg');
+      var escapedRelativePath = relativePath.replace(/(\/|\.)/g, '\\\\$1');
+      var escapedRelativeJpegPath = relativeJpegPath.replace(/(\/)/g, '\\\\$1');
+
+      var textPath = path.join(lfa.config.projectPath, 'text');
+      var stylesPath = path.join(lfa.config.projectPath, 'styles');
+      var jsPath = path.join(lfa.config.projectPath, 'js');
+
+      fixes.push('gm convert "' + fileName + '" "' + jpegPath + '"');
+      fixes.push('rm "' + fileName + '"');
+      fixes.push('find "' + textPath + '" -type f \\( -name "*.jade" \\) -exec sed -i "" "s/' + escapedRelativePath + '/' + escapedRelativeJpegPath + '/g" {} \\;');
+      fixes.push('find "' + stylesPath + '" -type f \\( -name "*.styl" -or -name "*.css" \\) -exec sed -i "" "s/' + escapedRelativePath + '/' + escapedRelativeJpegPath + '/g" {} \\;');
+      fixes.push('find "' + jsPath + '" -type f \\( -name "*.js" -or -name "*.jsx" -or -name "*.json" \\) -exec sed -i "" "s/' + escapedRelativePath + '/' + escapedRelativeJpegPath + '/g" {} \\;');
+      return;
+    }
+
   }
 
   lfa.task('assets:pre-write:file-size-checker', function (stream) {
@@ -31,42 +77,79 @@ module.exports = function fileSizeChecker(lfa) {
       return stream;
     }
 
-    return stream
-      .pipe(tap(function (file) {
-        if (!file.stat.isFile()) { 
-          return file;
+    var promises = [];
+
+    var returnStream = stream.pipe(tap(function (file) {
+      if (!file.stat.isFile()) { 
+        return file;
+      }
+
+      var actions = lfa.config.package.fileSizeCheckerActions || fileActions;
+
+      promises = promises.concat(_.map(actions, function (options, regexp) {
+        if (!new RegExp(regexp).test(file.path)) {
+          return;
         }
 
-        var actions = lfa.config.package.fileSizeCheckerActions || fileActions;
+        var fix = null;
+        var tooBig = false;
 
-        _.each(actions, function (options, regexp) {
-          if (!new RegExp(regexp).test(file.path)) {
-            return;
-          }
+        if (options.maxSize && file.stat.size >= options.maxSize) {
+          tooBig = true;
+          warn(file, 'File size (' + file.stat.size + ') bigger than ' + options.maxSize);
+        }
 
-          if (options.maxSize && file.stat.size >= options.maxSize) {
-            warn(file, 'File size (' + file.stat.size + ') bigger than ' + options.maxSize);
-          }
+        return when.try(function () {
+          if (!options.image) { return; }
 
-          if (options.image) {
-            gm(file.history[0]).size(function (err, size) {
-              if (err) {
-                warn(file, 'Can\'t open image:\n' + err.message);
-              } else {
-                if ((options.maxWidth && size.width > options.maxWidth) ||
-                    (options.maxHeight && size.height > options.maxHeight)) {
-                  warn(file, 'Image dimensions too big: ' + size.width + 'x' + size.height + ' bigger than ' + options.maxWidth + 'x' + options.maxHeight);
-                }
-                if ((options.minWidth && size.width < options.minWidth) ||
-                    (options.minHeight && size.height < options.minHeight)) {
-                  warn(file, 'Image dimensions too small: ' + size.width + 'x' + size.height + ' smaller than ' + options.minWidth + 'x' + options.minHeight);
-                }
+          var image = gm(file.history[0]);
+
+          return nodefn.call(image.identify.bind(image))
+            .then(function (data) {
+              if (tooBig && data.format === 'PNG') {
+                fix = 'pngcrush';
               }
-            });
-          }
-        });
 
-        return file;
+              var size = data.size;
+              if ((options.maxWidth && size.width > options.maxWidth) ||
+                  (options.maxHeight && size.height > options.maxHeight)) {
+                warn(file, 'Image dimensions too big: ' + size.width + 'x' + size.height + ' bigger than ' + options.maxWidth + 'x' + options.maxHeight);
+              }
+
+              if ((options.minWidth && size.width < options.minWidth) ||
+                  (options.minHeight && size.height < options.minHeight)) {
+                warn(file, 'Image dimensions too small: ' + size.width + 'x' + size.height + ' smaller than ' + options.minWidth + 'x' + options.minHeight);
+              }
+
+              var chStats = data['Channel Statistics'] || {};
+              if (data.format === 'PNG' && (!chStats.Opacity || parseNumber(chStats.Opacity.Minimum) === 1)) {
+                fix = 'tojpg';
+                warn(file, 'Opaque image saved as PNG');
+              }
+            })
+            .catch(function (err) {
+              warn(file, 'Can\'t open image:\n' + err.message);
+            });
+        }).then(function () {
+          applyFix(file, fix);
+        });
       }));
+
+      return file;
+    }));
+
+    returnStream.on('end', function () {
+      when.all(promises).then(function () {
+        if (fixes.length) {
+          lfa.logWarning('Saving a script with potential fixes in .lfa/build/fix.sh');
+          fixes.splice(0, 0, '#!/bin/bash');
+          var data = fixes.join('\n');
+          fs.writeFile(path.join(lfa.config.projectPath, '.lfa' ,'build', 'fix.sh'), data);
+        }
+      });
+    });
+
+    return returnStream;
   });
+
 };
